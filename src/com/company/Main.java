2bea3e1a715fe21fed0771b85398b719bd8ea497
vitask <skalenko.v@yandex.ru>
2022-03-19 13:03:04 +0700

    second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,8 +10,7 @@ import java.util.Collections;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        int [] array = new int [] {1,9,2,8,4,6,7,3};
-       /* BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        /*BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Введите размер массива");
         int arraySize = Integer.parseInt(reader.readLine());
         int[] array = new int[arraySize];
@@ -20,16 +19,27 @@ public class Main {
             System.out.println("Введите " + (i + 1) + "-ый элемент: ");
             array[i] = Integer.parseInt(reader.readLine());
         }
-        System.out.println("Ваш начальный массив: " + Arrays.toString(array));*/
-        ArrayList<ArrayList<Integer>> gis = groupIntegerSum(array, 3);
+        System.out.println("Выберите число групп");
+        int count = Integer.parseInt(reader.readLine());
+        ArrayList<ArrayList<Integer>> gis = groupIntegerSum(array, count);*/
 
-        for (ArrayList<Integer> group : gis) {
-            for (int i : group) {
-                System.out.print((i) + " ");
+        int [] array = new int [] {1,9,2,8,4,6,7,3};
+        ArrayList<ArrayList<Integer>> gis = groupIntegerSum(array,2);
+
+        try {
+            for (ArrayList<Integer> group : gis) {
+                for (int i : group) {
+                    System.out.print((i) + " ");
+                }
+                System.out.println();
             }
-            System.out.println();
         }
-    }
+            catch(NullPointerException e) {
+                System.out.println(e);
+
+            }
+        }
+
 
     private static ArrayList<ArrayList<Integer>> groupIntegerSum(int[] array, int counts) {
         Integer[] integerArr = new Integer[array.length];
@@ -37,27 +47,34 @@ public class Main {
             integerArr[i] = array[i];
         ArrayList<Integer> arrList = new ArrayList(Arrays.asList(integerArr));
         ArrayList<ArrayList<Integer>> gisList = new ArrayList<>();
+        System.out.println("Исходный массив:" + arrList);
         int sum = 0;
         for (int i : array)
             sum += i;
         System.out.println("Сумма чисел массива: " + sum);
-        if (sum % counts != 0)
-            System.out.println("Невозможно объеденить на равные группы");
         Collections.sort(arrList);
-        for (int i = 0; i < counts; i++) {
-            ArrayList<Integer> group = new ArrayList<>();
-            int res = 0;
-            int index = arrList.size() - 1;
-            while (res != sum / counts) {
-                if (res + arrList.get(index) <= sum / counts) {
-                    res += arrList.get(index);
-                    group.add(arrList.get(index));
-                    arrList.remove(index);
-                }
-                index--;
-            }
-            gisList.add(group);
+        if (sum % counts != 0)
+            System.out.println("Невозможно поделить на равные группы");
+        System.out.println("Отсортированный массив: " + arrList);
+        try {
+            for (int i = 0; i < counts; i++) {
+                ArrayList<Integer> group = new ArrayList<>();
+                int res = 0;
+                int index = arrList.size() - 1;
+                while (res != (sum / counts)) {
+                    if (res + arrList.get(index) <= (sum / counts)) {
+                        res += arrList.get(index);
+                        group.add(arrList.get(index));
+                        arrList.remove(index);
+                    }
+                    index--;
 
+                }
+                gisList.add(group);
+            }
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Не удалось создать на заданное количество групп");
         }
         return gisList;
     }
